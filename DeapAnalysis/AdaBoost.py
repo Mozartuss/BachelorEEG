c78da0d11c3bbffe6596fece60b769cc23bcacf2
Lukas Kleybolte <lukas.kleybolte@hs-augsburg.de>
2021-03-18 11:12:30 +0100

    Find best AdaBoost Parameter

--- DeapAnalysis/AdaBoost.py
+++ DeapAnalysis/AdaBoost.py
@@ -3,6 +3,7 @@ from os.path import sep
 
 import numpy as np
 import pandas as pd
+import xgboost as xgb
 from sklearn import metrics
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.model_selection import train_test_split
@@ -11,15 +12,13 @@ from xgboost import XGBClassifier
 
 
 def normal_abc(dataset, n, rate):
-    global best_abc
     x_train, x_test, y_train, y_test = dataset
     abc_normal = AdaBoostClassifier(n_estimators=n, learning_rate=rate)
     model_normal = abc_normal.fit(x_train, y_train)
     y_pred_normal = model_normal.predict(x_test)
     acc = metrics.accuracy_score(y_test, y_pred_normal)
-    print("AdaBoost with {} iterations and {} learning rate Accuracy:".format(n, rate), acc)
-    if (best_abc[-1] < acc):
-        best_abc = [n, rate, acc]
+    print("AdaBoost with\t {}\t iterations and\t {}\t learning rate -> Accuracy:".format(n, rate), np.round(acc, 2))
+    return [n, rate, acc]
 
 
 def normal_xgbc(dataset):
@@ -27,7 +26,19 @@ def normal_xgbc(dataset):
     xgbc = XGBClassifier(use_label_encoder=False)
     xgbc.fit(x_train, y_train)
     y_pred_xgbc = xgbc.predict(x_test)
-    print("XGBoost Accuracy:", metrics.accuracy_score(y_test, y_pred_xgbc))
+    print("XGBoost -> Accuracy:", np.round(metrics.accuracy_score(y_test, y_pred_xgbc), 2))
+
+
+def advanced_xgbr(dataset, depth, lr):
+    x_train, x_test, y_train, y_test = dataset
+    xg_reg = xgb.XGBRegressor(objective='reg:linear', colsample_bytree=0.3, learning_rate=lr, max_depth=depth,
+                              n_estimators=10)
+    xg_reg.fit(x_train, y_train)
+    y_pred_xgbr = xg_reg.predict(x_test)
+    acc = metrics.accuracy_score(y_test, y_pred_xgbr)
+    print("XGBoost advanced with depth\t {}\t and\t {}\t learning rate -> Accuracy:".format(depth, lr),
+          np.round(acc, 2))
+    return [depth, lr, acc]
 
 
 def svc_abc(dataset, kernel, decision):
@@ -37,8 +48,8 @@ def svc_abc(dataset, kernel, decision):
                                  learning_rate=0.01)
     model_svc = abc_svc.fit(x_train, y_train)
     y_pred_svc = model_svc.predict(x_test)
-    print("AdaBoost svc with kernel {} and {} decision Accuracy:".format(kernel, decision),
-          metrics.accuracy_score(y_test, y_pred_svc))
+    print("AdaBoost svc with kernel\t {}\t and\t {}\t decision -> Accuracy:".format(kernel, decision),
+          np.round(metrics.accuracy_score(y_test, y_pred_svc), 2))
 
 
 def load_test_data(data_path, label_path, bins, type):
@@ -52,23 +63,29 @@ def load_test_data(data_path, label_path, bins, type):
 
 if __name__ == '__main__':
     path = ".." + sep + ".." + sep + ".." + sep + "DEAP Dataset" + sep + "Preprocessed Datasets" + sep + "data_preprocessed_python" + sep + "Participants" + sep + "CompleteFeatures" + sep
-    data = "CompleteFeatures.csv"
+    data = "SelectedFeaturesArousal.csv"
     label = "CompleteFeaturesLabels.csv"
     # bins = -2=[0-1] -1=[1-2.25] 0=[2.25-5] 1=[5-7.25] 2=[7.25-9.9999] -> scale Label from -2 to 2
     X, y = load_test_data(path + data, path + label, np.array([1, 2.25, 5, 7.25, 9]), "arousal")
 
-    rates = np.arange(0.01, 1.05, 0.05)
-    iters = np.arange(500, 10000, 250)
-    best_abc = [0, 0, 0]
+    rates = np.arange(0.01, 1.00, 0.01)
+    iters = np.arange(500, 100000, 250)
+    dt = train_test_split(X, y, test_size=0.3)
 
-    pool = mp.Pool(mp.cpu_count())
-    result = pool.starmap(normal_abc,
-                          [(train_test_split(X, y, test_size=0.3), n, np.round(rate, 2)) for n in iters for rate in
-                           rates])
-    result = sorted(result, key=lambda x: x[-1], reverse=True)
-    print(result[0])
-    pool.apply(normal_xgbc, args=(train_test_split(X, y, test_size=0.3)))
+    pool = mp.Pool(mp.cpu_count() - 1)
+    result_abc = pool.starmap(normal_abc,
+                              [(dt, n, np.round(rate, 2)) for n in iters for rate in
+                               rates])
+    result_abc = sorted(result_abc, key=lambda x: x[-1], reverse=True)
+    print(result_abc[0])
+    pool.apply(normal_xgbc, args=dt)
+
+    result_xgbr = pool.starmap(advanced_xgbr,
+                               [(dt, depth, np.round(rate, 2)) for depth in np.arange(5, 1280, 5) for rate in
+                                rates])
+    result_xgbr = sorted(result_xgbr, key=lambda x: x[-1], reverse=True)
+    print(result_xgbr[0])
     pool.starmap(svc_abc,
-                 [(train_test_split(X, y, test_size=0.3), k, d) for k in ["linear", "rbf", "poly", "sigmoid"] for d in
+                 [(dt, k, d) for k in ["linear", "rbf", "poly", "sigmoid"] for d in
                   ["ovo", "ovr"]])
     pool.close()
